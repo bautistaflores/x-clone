d66dc4e32f29bfb49064d1b1bf9bbdc66af9fa30
bautistaflores <bautistaflores22@gmail.com>
2025-08-08 01:33:48 -0300

    feat: subir img a post (solo frontend)  - Boton para subir img a un post  - Preview de la img

--- client/src/components/ComposePost.jsx
+++ client/src/components/ComposePost.jsx
@@ -1,22 +1,41 @@
 import { useNavigate, useLocation } from "react-router-dom";
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import { usePosts } from "../context/PostsContext";
+import { useAuth } from "../context/AuthContext";
+import { useProfiles } from "../context/ProfilesContext";
 import CloseIcon from "./Icons/CloseIcon";
+import ImgIcon from "./Icons/imgIcon";
 
 const MAX_CHARACTERS = 280;
 
 function ComposePost() {
+    // context
     const { createPost } = usePosts()
+    const { user, isAuthenticated } = useAuth()
+    const { profile, getProfile } = useProfiles()
+
+    // hooks
     const navigate = useNavigate()
     const location = useLocation()
-    const textareaRef = useRef(null)
 
+    // refs
+    const textareaRef = useRef(null)
+    const fileInputRef = useRef(null)
     const [content, setContent] = useState('');
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [previewImage, setPreviewImage] = useState(null);
 
     const remainingCharacters = MAX_CHARACTERS - content.length;
 
     const isButtonDisabled = content.trim().length === 0 || content.length > MAX_CHARACTERS;
 
+    useEffect(() => {
+        if (isAuthenticated) {
+            getProfile(user?.username)
+        }
+    }, [isAuthenticated, user?.username, getProfile])
+
+    // cerrar modal
     const handleCloseModal = () => {
         if (location.state && location.state.background) {
             navigate(-1)
@@ -25,6 +44,7 @@ function ComposePost() {
         }
     }
 
+    // cambiar contenido del textarea
     const handleContentChange = (e) => {
         const content = e.target.value;
         if (content.length <= MAX_CHARACTERS) {
@@ -36,6 +56,22 @@ function ComposePost() {
         }
     }
 
+    const handleImageChange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            setSelectedImage(file);
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setPreviewImage(reader.result);
+            };
+            reader.readAsDataURL(file);
+        } else {
+            setSelectedImage(null);
+            setPreviewImage(null);
+        }
+    }
+
+    // postear
     const handlePostSubmit = async () => {
         if (!isButtonDisabled) {
             await createPost({ content })
@@ -44,41 +80,90 @@ function ComposePost() {
         }
     }
 
+    // boton svg
+    const handleIconClick = () => {
+        fileInputRef.current.click();
+    }
+
     return (
         // fondosemi-transparente
         <div style={{ backgroundColor: 'rgba(91, 112, 131, 0.4)' }} className="fixed inset-0 flex items-start justify-center pt-10 z-50">
             <div className="bg-black text-white rounded-2xl p-3 w-full max-w-xl">
                 {/* boton cerrar */}
                 <div>
+                    <input type="file" accept="image/*" className="hidden" />
                     <button onClick={handleCloseModal} className="cursor-pointer hover:bg-gray-900 rounded-full p-2">
                         <CloseIcon />
                     </button>
                 </div>
 
-                {/* formulario */}
-                <div className="py-4">
-                    <textarea 
-                        ref={textareaRef}
-                        className="w-full pb-5 bg-transparent text-lg focus:outline-none placeholder-gray-500 resize-none overflow-hidden" 
-                        placeholder="¿Qué está pasando?"
-                        value={content}
-                        onChange={handleContentChange}
-                    ></textarea>
+                <div className="flex gap-3 mt-4 ml-2">
+                    <div className="flex-shrink-0 w-auto">
+                        {profile?.profile?.profile_picture && (
+                            <img 
+                                src={profile?.profile?.profile_picture} 
+                                alt={profile?.username}
+                                className="w-10 h-10 rounded-full"
+                            />
+                        )}
+                    </div>
+
+                    {/* formulario */}
+                    <div className="mt-1 w-full">
+                        <textarea 
+                            ref={textareaRef}
+                            className={`w-full bg-transparent text-xl focus:outline-none placeholder-gray-500 resize-none overflow-hidden ${previewImage ? 'pb' : 'pb-12'}`} 
+                            placeholder="¿Qué está pasando?"
+                            value={content}
+                            onChange={handleContentChange}
+                        ></textarea>
+
+                        {/* imagen previsualizada */}
+                        {previewImage && (
+                            <div className="relative mb-4">
+                                <img src={previewImage} alt="Preview" className="w-full h-auto rounded-xl" />
+                                {/* boton eliminar imagen */}
+                                <button
+                                    onClick={() => {
+                                        setSelectedImage(null);
+                                        setPreviewImage(null);
+                                        if (fileInputRef.current) {
+                                            fileInputRef.current.value = "";
+                                        }
+                                    }}
+                                    className="absolute top-1 right-1 cursor-pointer bg-black/50 hover:bg-gray-800/50 transition-colors duration-200 rounded-full p-1.5"
+                                >
+                                    <CloseIcon height={18} width={18}/>
+                                </button>
+                            </div>
+                        )}
+                    </div>
                 </div>
 
-                {/* boton postear */}
-                <div className="flex justify-end">
-                    <span className={`text-sm mr-4 ${remainingCharacters < 15 ? 'text-red-500' : 'text-gray-500'}`}>
-                        {remainingCharacters}
-                    </span>
-                    <button 
-                        className={`bg-white text-black font-bold py-2 px-6 rounded-full transition-colors duration-200 
-                            ${isButtonDisabled ? 'opacity-50' : 'hover:bg-gray-200 cursor-pointer'}`}
-                        disabled={isButtonDisabled}
-                        onClick={handlePostSubmit}
-                    >
-                        Postear
-                    </button>
+                {/* boton postear y subir imagen */}
+                <div className="flex justify-between items-center border-t border-gray-500/50 pt-2">
+                    {/* boton subir imagen */}
+                    <div>
+                        <input type="file" accept="image/*" className="hidden" ref={fileInputRef} onChange={handleImageChange}/>
+                        <div className="cursor-pointer hover:bg-blue-900/20 rounded-full p-2" onClick={handleIconClick}>
+                            <ImgIcon height={22} width={22}/>
+                        </div>
+                    </div>
+
+                    {/* boton postear */}
+                    <div>    
+                        <span className={`text-sm mr-4 ${remainingCharacters < 15 ? 'text-red-500' : 'text-gray-500'}`}>
+                            {remainingCharacters}
+                        </span>
+                        <button 
+                            className={`bg-white text-black font-bold py-2 px-6 rounded-full transition-colors duration-200 
+                                ${isButtonDisabled ? 'opacity-50' : 'hover:bg-gray-200 cursor-pointer'}`}
+                            disabled={isButtonDisabled}
+                            onClick={handlePostSubmit}
+                        >
+                            Postear
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
