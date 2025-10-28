94ea74531f04c9ecfaa5036af1f2a89b52801d9a
bautistaflores <bautistaflores22@gmail.com>
2025-10-28 14:21:18 -0300

    fix(redis): Configurar TLS basado en la URL

--- services/auth-service/src/libs/redis.js
+++ services/auth-service/src/libs/redis.js
@@ -3,9 +3,20 @@ import logger from '../libs/logger.js';
 import config from '../config.js';
 
 // Configuración de Redis
-const redisClient = createClient({
+const redisConfig = {
   url: config.REDIS_URL
-});
+};
+
+// Configuración de Redis para HTTPS en Render
+if (config.REDIS_URL.startsWith('rediss://')) {
+  redisConfig.socket = {
+    tls: true,
+    rejectUnauthorized: false
+  };
+}
+
+// Crea el cliente de Redis
+const redisClient = createClient(redisConfig);
 
 // Maneja errores de Redis
 redisClient.on('error', (err) => logger.error(`Redis error: ${err.message}`));
